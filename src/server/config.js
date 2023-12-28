f3e215091c421eb0e685e03ca239be207eca0019
Alejandro <alejandroxd25k@gmail.com>
2023-12-28 07:23:47 -0500

    Change in Frontend

--- src/server/config.js
+++ src/server/config.js
@@ -9,7 +9,7 @@ const errorHandler = require('errorhandler');
 
 module.exports = (app) =>{
 
-    app.set('port' , process.env.PORT || 3000);
+    app.set('port' , process.env.PORT || 3030);
     app.set('views' ,path.join(__dirname , '../views') );
     const hbs = exphbs.create({
         defaultLayout: 'main',
