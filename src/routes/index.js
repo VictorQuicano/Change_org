568e780081fa028cc460a7a240a4798e8ada8f1e
Alejandro <alejandroxd25k@gmail.com>
2023-12-28 12:30:42 -0500

    Solve

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,8 @@ module.exports= app => {
     router.post('/images' , image.create) 
     router.post('/images/:image_id/like' , image.like) 
     router.post('/images/:image_id/comment' , image.comment) 
-    router.delete('/images/:image_id' , image.remove) 
+    router.delete('/images/:image_id' , image.remove)
+    router.get('/create' , home.create) 
     //router.get('/images/:image_id/donate' , image.donate)
     router.post('/images/:image_id/donate' , image.donate)
 
